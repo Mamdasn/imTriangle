097975327decf2f547542f8510aaa4e261a8ea51
mamdasn <mhsaber1375@gmail.com>
2021-05-02 18:47:04 +0430

    first commit

---
+++ code-example/test_imTriangle.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+from imTriangle import triangle
+import os
+
+
+img_fullname = 'assets/lotus.jpg'
+img_name, img_ext = os.path.splitext(img_fullname)
+
+img = cv2.imread(img_fullname)
+img_out = img.copy()
+
+[h, w, d] = img.shape
+for i in range(d):
+    img_out[:, :, i] = triangle(img[:, :, i].copy())
+
+
+    
+cv2.imwrite(f'{img_name}-triangled{img_ext}', img_out)
+
+
+
